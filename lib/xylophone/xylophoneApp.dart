41e88234bea29025f99b69f3f1e50c9b3e115597
ZaidZakir <zaidzakir@hlbb.hongleong.com.my>
2021-06-18 23:40:11 +0800

    Add multiple buttons to play all sounds

--- lib/xylophone/xylophoneApp.dart
+++ lib/xylophone/xylophoneApp.dart
@@ -15,6 +15,11 @@ class XylophoneApp extends StatefulWidget {
 }
 
 class _XylophoneAppState extends State<XylophoneApp> {
+  void playSound(String note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,16 +28,50 @@ class _XylophoneAppState extends State<XylophoneApp> {
         title: Text('Xylophone App'),
         backgroundColor: Colors.black,
       ),
-      body: SafeArea(child:
-          Center(
-            child: TextButton(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TextButton(
               onPressed: (){
-                final player = AudioCache();
-                player.play('note1.wav');
+                playSound("1");
               },
-              child: Text('Play sound'),
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
             ),
-          ),
+            TextButton(
+              onPressed: (){
+                playSound("2");
+              },
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue)),
+            ),
+            TextButton(
+              onPressed: (){
+                playSound("3");
+              },
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
+            ),TextButton(
+              onPressed: (){
+                playSound("4");
+              },
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellow)),
+            ),
+            TextButton(
+              onPressed: (){
+                playSound("5");
+              },
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.purpleAccent)),
+            ),TextButton(
+              onPressed: (){
+                playSound("6");
+              },
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.deepOrange)),
+            ),TextButton(
+              onPressed: (){
+                playSound("7");
+              },
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.brown)),
+            )
+          ],
+        ),
       ),
     );
   }
